0fe305d81ea833c30ec6b0eeba12435c48f30070
Diego Câmara <diegodscamara@gmail.com>
2025-01-05 21:42:36 -0500

    Enhance TitleSection component to support class status handling
    - Added support for displaying the title input conditionally based on the selected class's status (completed, cancelled).
    - Implemented logic to render a read-only title when the class is immutable, improving user experience.
    - Updated styling and structure for better layout and accessibility.

--- components/dashboard/class-modal/TitleSection.tsx
+++ components/dashboard/class-modal/TitleSection.tsx
@@ -1,23 +1,32 @@
-import { NotePencil } from "@phosphor-icons/react";
+import { Class } from "@/types/class";
+import { TextAlignLeft } from "@phosphor-icons/react";
 
 interface TitleSectionProps {
   title: string;
   onChange: (title: string) => void;
+  selectedClass?: Class;
 }
 
-const TitleSection = ({ title, onChange }: TitleSectionProps) => {
+const TitleSection = ({ title, onChange, selectedClass }: TitleSectionProps) => {
+  const isImmutable = selectedClass?.status === 'completed' || selectedClass?.status === 'cancelled';
+
   return (
-    <div className="p-4">
-      <div className="flex items-center gap-3">
-        <NotePencil className="w-5 h-5 text-base-content/70" />
-        <input
-          type="text"
-          className="focus:bg-transparent px-2 w-full font-medium text-lg input input-primary input-sm"
-          value={title}
-          onChange={(e) => onChange(e.target.value)}
-          placeholder="Add title"
-          required
-        />
+    <div className="space-y-4 p-4">
+      <div className="flex items-start gap-3">
+        <TextAlignLeft className="mt-3 w-5 h-5 text-base-content/70" />
+        <div className="flex-1">
+          {isImmutable ? (
+            <p className="mt-2 text-base-content text-lg">{title}</p>
+          ) : (
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => onChange(e.target.value)}
+              placeholder="Class Title"
+              className="input-bordered w-full input"
+            />
+          )}
+        </div>
       </div>
     </div>
   );
