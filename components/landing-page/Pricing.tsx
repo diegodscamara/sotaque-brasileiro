9522affd7c3806e90fbfa5fce58ecef7c616d17f
Diego Câmara <diegodscamara@gmail.com>
2025-01-19 11:32:13 -0500

    Update pricing plans in configuration and enhance UI components
    - Refactored pricing plan descriptions to remove redundant information and improve clarity.
    - Added specific features for each plan to enhance user understanding of offerings.
    - Updated the ButtonCheckout component to utilize a new UI button for better styling and interaction.
    - Enhanced the Pricing component with a tabbed interface for better organization of plan intervals.
    - Introduced new UI elements such as Badge and Card for improved visual presentation of pricing plans.
    - Cleaned up unused imports and optimized component structure for maintainability.

--- components/landing-page/Pricing.tsx
+++ components/landing-page/Pricing.tsx
@@ -1,5 +1,9 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { Badge } from "@/components/ui/badge";
 import ButtonCheckout from "../ButtonCheckout";
-import PricingTabs from "./PricingTabs";
+import { Card } from "../ui/card";
+import { CheckCircle } from "lucide-react";
 import config from "@/config";
 
 // <Pricing/> displays the pricing plans for your app
@@ -13,111 +17,101 @@ const Pricing = () => {
     new Set(config.stripe.plans.map((plan) => plan.interval))
   );
 
-  const tabs = intervals.map((interval) => ({
-    id: interval,
-    title: interval.charAt(0).toUpperCase() + interval.slice(1),
-    content: (
-      <div className="relative flex flex-col items-center justify-center gap-8 lg:flex-row lg:items-stretch">
-        {config.stripe.plans
-          .filter((plan) => plan.interval === interval)
-          .map((plan) => (
-            <div key={plan.priceId} className="relative w-full max-w-lg">
-              {plan.isFeatured && (
-                <div className="absolute top-0 z-20 -translate-x-1/2 -translate-y-1/2 left-1/2">
-                  <span
-                    className={`badge text-xs text-primary-content font-semibold border-0 bg-accent`}
-                  >
-                    POPULAR
-                  </span>
-                </div>
-              )}
-
-              {plan.isFeatured && (
-                <div
-                  className={`absolute -inset-[1px] rounded-[9px] bg-accent z-10`}
-                ></div>
-              )}
-
-              <div className="relative z-10 flex flex-col h-full gap-5 p-8 rounded-lg lg:gap-8 bg-base-100">
-                <div className="flex items-center justify-between gap-4">
-                  <div>
-                    <p className="text-lg font-bold lg:text-xl">{plan.name}</p>
-                    {plan.description && (
-                      <p className="mt-2 text-base-content/80">
-                        {plan.description}
-                      </p>
-                    )}
-                  </div>
-                </div>
-                <div className="flex gap-2">
-                  {plan.priceAnchor && (
-                    <div className="flex flex-col justify-end mb-[4px] text-lg ">
-                      <p className="relative">
-                        <span className="absolute bg-base-content h-[1.5px] inset-x-0 top-[53%]"></span>
-                        <span className="text-base-content/80">
-                          ${plan.priceAnchor}
-                        </span>
-                      </p>
-                    </div>
-                  )}
-                  <p className={`text-5xl tracking-tight font-extrabold`}>
-                    ${plan.price}
-                  </p>
-                  <div className="flex flex-col justify-end mb-[4px]">
-                    <p className="text-xs font-semibold uppercase text-base-content/60">
-                      CAD
-                    </p>
-                  </div>
-                </div>
-                {plan.features && (
-                  <ul className="space-y-2.5 leading-relaxed text-base flex-1">
-                    {plan.features.map((feature, i) => (
-                      <li key={i} className="flex items-center gap-2">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 20 20"
-                          fill="currentColor"
-                          className="w-[18px] h-[18px] opacity-80 shrink-0"
-                        >
-                          <path
-                            fillRule="evenodd"
-                            d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                            clipRule="evenodd"
-                          />
-                        </svg>
-
-                        <span>{feature.name} </span>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-                <div className="space-y-2">
-                  <ButtonCheckout
-                    priceId={plan.priceId}
-                    mode={getMode(plan.interval)}
-                  />
-                  <p className="relative flex items-center justify-center gap-2 text-sm font-medium text-center text-base-content/80">
-                    Pay once. Access forever.
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-      </div>
-    ),
-  }));
-
   return (
-    <section className="overflow-hidden bg-base-200" id="pricing">
-      <div className="max-w-5xl px-8 py-24 mx-auto">
-        <div className="flex flex-col w-full mb-20 text-center">
-          <p className="mb-8 font-medium text-primary">Pricing</p>
-          <h2 className="text-3xl font-bold tracking-tight lg:text-5xl">
-            Save hours of repetitive code and ship faster!
-          </h2>
+    <section className="container" id="pricing">
+      <div className="mx-auto py-24">
+        <div className="flex flex-col gap-4 mb-20 w-full text-center">
+          <h2 className="font-bold text-3xl lg:text-5xl tracking-tight">Pricing</h2>
+          <p className="mx-auto w-full max-w-lg font-large text-base-content/80">
+            We strive to provide the best service possible. This is why we offer a 30-day money back guarantee if you are not happy with our service
+          </p>
+          <Badge variant="outline" className="mx-auto w-fit">Save up to 17% with annual plans!</Badge>
         </div>
 
-        <PricingTabs tabs={tabs} />
+        <Tabs defaultValue={intervals[0]} className="flex flex-col items-center gap-8 w-full">
+          <TabsList>
+            {intervals.map((interval) => (
+              <TabsTrigger key={interval} value={interval}>
+                {interval.charAt(0).toUpperCase() + interval.slice(1)}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          {intervals.map((interval) => (
+            <TabsContent key={interval} value={interval}>
+              <div className="relative flex lg:flex-row flex-col justify-center items-center lg:items-stretch gap-8">
+                {config.stripe.plans
+                  .filter((plan) => plan.interval === interval)
+                  .map((plan) => (
+                    <Card key={plan.priceId} className={`relative w-full max-w-lg ${plan.isFeatured ? "border-primary" : ""}`}>
+                      {plan.isFeatured && (
+                        <div className="top-0 left-1/2 z-20 absolute -translate-x-1/2 -translate-y-1/2">
+                          <Badge>
+                            POPULAR
+                          </Badge>
+                        </div>
+                      )}
+
+                      <div className="relative z-10 flex flex-col gap-5 lg:gap-8 bg-base-100 p-8 rounded-lg h-full">
+                        <div className="flex flex-col justify-between items-start gap-4">
+                          <p className="font-bold text-lg lg:text-xl">{plan.name}</p>
+                          {plan.description && (
+                            <p className="text-base-content/50">
+                              {plan.description}
+                            </p>
+                          )}
+                        </div>
+                        <div className="flex flex-col justify-end gap-2">
+                          <div className="flex gap-2">
+                            {plan.priceAnchor && (
+                              <div className="flex flex-col justify-end mb-[4px] text-lg">
+                                <p className="relative">
+                                  <span className="top-[53%] absolute inset-x-0 bg-base-content h-[1.5px]"></span>
+                                  <span className="text-base-content/80">
+                                    ${plan.priceAnchor}
+                                  </span>
+                                </p>
+                              </div>
+                            )}
+                            <p className={`text-5xl tracking-tight font-extrabold`}>
+                              ${plan.interval === "monthly" ? plan.price : plan.price / 12}
+                            </p>
+                            <div className="flex flex-col justify-end mb-[4px]">
+                              <p className="text-base-content/60 text-xs">
+                                /month
+                              </p>
+                            </div>
+                          </div>
+                          <p className="text-base-content/60 text-xs">
+                            {plan.interval === "monthly"
+                              ? "Billed monthly"
+                              : "Billed annually"}
+                          </p>
+                        </div>
+
+                        <ButtonCheckout
+                          priceId={plan.priceId}
+                          mode={getMode(plan.interval)}
+                          variant={plan.isFeatured ? "default" : "outline"}
+                        />
+
+                        {plan.features && (
+                          <ul className="space-y-2.5 text-base">
+                            {plan.features.map((feature, i) => (
+                              <li key={i} className="flex items-center gap-2">
+                                <CheckCircle className="w-4 h-4" />
+                                <span>{feature.name} </span>
+                              </li>
+                            ))}
+                          </ul>
+                        )}
+                      </div>
+                    </Card>
+                  ))}
+              </div>
+            </TabsContent>
+          ))}
+        </Tabs>
       </div>
     </section>
   );
