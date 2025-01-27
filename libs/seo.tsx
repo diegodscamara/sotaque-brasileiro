b400c8bc83269ad3dc38b9da1e6184d2f4d733c8
Diego Câmara <diegodscamara@gmail.com>
2025-01-26 19:11:41 -0500

    Update config and SEO metadata for Sotaque Brasileiro
    - Revise app description to highlight personalized language learning
    - Update pricing plan features to be more concise and focused
    - Modify email and support contact information
    - Update SEO metadata, including Twitter creator and schema.org details
    - Adjust domain and email configurations

--- libs/seo.tsx
+++ libs/seo.tsx
@@ -55,7 +55,7 @@ export const getSEOTags = ({
       // If you add an twitter-image.(jpg|jpeg|png|gif) image to the /app folder, you don't need the code below
       // images: [openGraph?.image || defaults.og.image],
       card: "summary_large_image",
-      creator: "@marc_louvion",
+      creator: "@sotaquebrasileiro",
     },
 
     // If a canonical URL is given, we add it. The metadataBase will turn the relative URL into a fully qualified URL
@@ -89,9 +89,9 @@ export const renderSchemaTags = () => {
           url: `https://${config.domainName}/`,
           author: {
             "@type": "Person",
-            name: "Marc Lou",
+            name: "Sotaque Brasileiro",
           },
-          datePublished: "2023-08-01",
+          datePublished: "2025-01-27",
           applicationCategory: "EducationalApplication",
           aggregateRating: {
             "@type": "AggregateRating",
@@ -101,8 +101,29 @@ export const renderSchemaTags = () => {
           offers: [
             {
               "@type": "Offer",
-              price: "9.00",
+              price: "260.00",
               priceCurrency: "USD",
+              availability: "https://schema.org/InStock",
+              seller: {
+                "@type": "Organization",
+                name: "Sotaque Brasileiro",
+              },
+            },
+            {
+              "@type": "Offer",
+              price: "380.00",
+              priceCurrency: "USD",
+              availability: "https://schema.org/InStock",
+              seller: {
+                "@type": "Organization",
+                name: "Sotaque Brasileiro",
+              },
+            },
+            {
+              "@type": "Offer",
+              price: "450.00",
+              priceCurrency: "USD",
+              availability: "https://schema.org/InStock",
             },
           ],
         }),
